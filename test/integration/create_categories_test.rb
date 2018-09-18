edbd447352a06bb50a741427e0af04dc82af8d96
Ankur Tripathi <ankur456799@gmail.com>
2018-09-18 22:00:11 +0530

    Isse reproduce

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
-class CategoriesControllerTest < ActionDispatch::IntegrationTest
+class CreateCategoriesControllerTest < ActionDispatch::IntegrationTest
+	
+	def setup
+		@user = User.create(username: "john", email: "john@example.com", password: "password", admin: true)
+	end
+
 	test "get new category form and create category" do
+		sign_in_as(@user, "password")
 		get new_category_path
 		assert_template 'categories/new'
 		assert_difference 'Category.count', 1 do
@@ -13,13 +19,14 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
 	end
 
 	test "invalid category submission in result display error" do
+		sign_in_as(@user, "password")
 		get new_category_path
 		assert_template 'categories/new'
 		assert_no_difference 'Category.count' do
 			post categories_path, params: { category: { name: " " } }
 		end
 		assert_template 'categories/new'
-		#assert_select 'h2.panel-title'
-		#assert_select 'div.panel-body'
+		assert_select 'h2.panel-title'
+		assert_select 'div.panel-body'
 	end
 end
